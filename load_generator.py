c50579c36375d7485f3cf96f369e81f664c3f000
Matheus Sodre <matheusssodre98@gmail.com>
2025-05-17 14:29:24 -0300

    Atualizando configuração do Docker e Load Generator

--- load_generator.py
+++ load_generator.py
@@ -1,43 +1,11 @@
-import csv
-import json
-from faker import Faker
-import random
-from concurrent.futures import ThreadPoolExecutor
+from file_generator import salvar_json, salvar_csv
 
-fake = Faker()
-
-def gerar_massa_carga(quantidade: int = 10000) -> list[dict[str, str | float | None]]:
-    """Gera uma lista de dicionários com dados fake para testes de carga."""
-    dados = []
-    for _ in range(quantidade):
-        dados.append({
-            "ID": fake.uuid4(),
-            "Nome": fake.name(),
-            "Email": fake.email(),
-            "Endereço": fake.address(),
-            "Telefon": fake.phone_number(),
-            "Data de Nascimento": fake.date_of_birth().strftime("%Y-%m-%d"),
-            "Empresa": fake.company(),
-            "Salário": round(random.uniform(2000, 15000), 2)
-        })
-    return dados
-
-def salvar_csv(nome_arquivo="massa_carga.csv", quantidade=10000):
-    dados = gerar_massa_carga(quantidade)
-    with open(nome_arquivo, mode='w', newline='', encoding='utf-8') as arquivo_csv:
-        writer = csv.DictWriter(arquivo_csv, fieldnames=dados[0].keys())
-        writer.writeheader()
-        writer.writerows(dados)
-    print(f"✅ Arquivo CSV '{nome_arquivo}' gerado!")
-
-def salvar_json(nome_arquivo="massa_carga.json", quantidade=10000):
+def gerar_e_salvar_testes():
+    """Gera os testes de carga e salva os arquivos."""
+    json_path = "/app/downloads/massa_carga.json"
+    csv_path = "/app/downloads/massa_carga.csv"
     
-    dados = gerar_massa_carga(quantidade)
-    with open(nome_arquivo, mode='w', encoding='utf-8') as arquivo_json:
-        json.dump(dados, arquivo_json, indent=4, ensure_ascii=False)
-    print(f"✅ Arquivo JSON '{nome_arquivo}' gerado!")
+    salvar_json(json_path, 10000)
+    salvar_csv(csv_path, 10000)
 
-if __name__ == "__main__":
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        executor.submit(salvar_csv, "massa_carga.csv", 10000)
-        executor.submit(salvar_json, "massa_carga.json", 10000)
+gerar_e_salvar_testes()
